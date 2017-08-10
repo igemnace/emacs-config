c55280f0cc9ceb1731159cac9df938df54f50b7b
Ian Emnace <igemnace@gmail.com>
2017-08-10 10:51:58 +0800

    Initial commit
    Repo already contains a LICENSE and README, with a simple script to
    install the .emacs.d directory from cfg.
    
    The meatiest part of the repo for now is my init.el, which already has
    some configuration. Some nice packages are also now installed.
    
    This repo was even checked in from Emacs via Magit, with the README
    edited with markdown-mode and viewed with gh-md.
    
    And so, my Emacs journey begins.
    
    ...this is fun. I should start moving to descriptive commit messages
    for my other config repos.

---
+++ README.md
@@ -0,0 +1,25 @@
+# my-emacs-config
+
+Easy way to transfer my Emacs configuration across multiple machines.
+
+## Pre-Installation
+
+Make sure that your machine satisfies the following:
+- Is using GNU/Linux (scripts are untested on other UNIX systems and will
+  flat-out fail on Windows)
+- Has Emacs 24 or higher (required by ELPA and the package system)
+- Has no pre-existing `.emacs.d` (backup and remove if existing)
+- Has env, bash, and git (required by install scripts)
+- Has a working internet connection (will be installing packages)
+
+## Installation
+
+Just run `./install`. This will automatically do the following:
+- Symlink `.emacs.d` into your home directory (`install-emacsd`)
+
+## Licensing
+
+If anyone else sees this and wants to use it, go ahead.
+
+This project is licensed under the Unlicense and is entirely under public
+domain.
---
+++ cfg/.emacs.d/init.el
@@ -0,0 +1,64 @@
+;; Package Setup
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives
+	     '("melpa-stable" . "http://stable.melpa.org/packages/"))
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+;; Magit
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
+
+;; SLIME
+(setq inferior-lisp-program "/usr/bin/sbcl")
+(add-to-list 'load-path "/usr/share/emacs/site-lisp/slime/")
+(require 'slime)
+(slime-setup)
+
+;; Helm
+(require 'helm)
+(global-set-key (kbd "C-x b") 'helm-buffers-list)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x C-f") 'helm-for-files)
+
+;; ParEdit
+(autoload #'enable-paredit-mode "paredit" "Enable paredit." t)
+(add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook #'enable-paredit-mode)
+
+;; Theme
+(load-theme 'base16-eighties t)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(toggle-scroll-bar -1)
+(add-to-list 'default-frame-alist '(font . "Roboto Mono Light-9"))
+
+;; Backup files behavior
+(setq
+   backup-by-copying t
+   backup-directory-alist '(("." . "~/.emacs.d/tmp"))
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2
+   version-control t)
+
+;; Auto-generated
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("9be1d34d961a40d94ef94d0d08a364c3d27201f3c98c9d38e36f10588469ea57" default)))
+ '(icomplete-mode nil)
+ '(package-selected-packages
+   (quote
+    (gh-md markdown-mode company base16-theme flycheck magit paredit slime helm))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
