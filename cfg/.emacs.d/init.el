69d96e872055b292b18b4143c122e004a8f56704
Ian Emnace <igemnace@gmail.com>
2017-09-08 10:22:11 +0800

    Fix setq indentation
    Lisp buffers seem to rely on indentation that aligns sexps nicely. I
    just ran my setq block through fill-paragraph to fix it.

--- cfg/.emacs.d/init.el
+++ cfg/.emacs.d/init.el
@@ -60,12 +60,12 @@
 
 ;; Backup files behavior
 (setq
-   backup-by-copying t
-   backup-directory-alist '(("." . "~/.emacs.d/tmp"))
-   delete-old-versions t
-   kept-new-versions 6
-   kept-old-versions 2
-   version-control t)
+ backup-by-copying t
+ backup-directory-alist '(("." . "~/.emacs.d/tmp"))
+ delete-old-versions t
+ kept-new-versions 6
+ kept-old-versions 2
+ version-control t)
 
 ;; Auto-generated
 (custom-set-variables
