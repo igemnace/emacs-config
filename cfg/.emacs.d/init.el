2c71f19f040f70fa7dbc90f779716ae5649c6bb0
Ian Emnace <igemnace@gmail.com>
2024-12-14 13:16:44 +0800

    Overhaul init.el
    I'm returning to Emacs because I wanna give org another fair
    shake. I'm yet again trying to resist going for evil, because some
    quick tests show that I won't be able to use the keybinds listed in
    the manual (e.g. M-RET and that sort of thing).
    
    I'm coming at this from the perspective of only needing to edit Org
    and Elisp files, so I'm not trying to set up an entire config for
    development. Instead, I'm just setting up Org and Magit, then a bunch
    of niceties like fuzzy finding and theming.

--- cfg/.emacs.d/init.el
+++ cfg/.emacs.d/init.el
@@ -1,104 +1,60 @@
-;; Package Setup
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives
-	     '("melpa-stable" . "http://stable.melpa.org/packages/"))
-(setq package-enable-at-startup nil)
-(package-initialize)
+             '("melpa" . "https://melpa.org/packages/") t)
+(eval-when-compile
+  (require 'use-package))
 
-;; Magit
-(global-set-key (kbd "C-x g") 'magit-status)
-(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
+(use-package magit
+  :ensure t)
 
-;; SLIME
-(setq inferior-lisp-program "/usr/bin/sbcl")
-(add-to-list 'load-path "/usr/share/emacs/site-lisp/slime/")
-(require 'slime)
-(slime-setup)
+(use-package org
+  :ensure t
+  :bind (("C-c l" . 'org-store-link)
+	 ("C-c a" . 'org-agenda)
+	 ("C-c c" . 'org-capture))
+  :config (setq org-agenda-files '("~/org")))
 
-;; Ivy
-(require 'ivy)
-(ivy-mode 1)
+(use-package vertico
+  :ensure t
+  :config (vertico-mode))
 
-;; Counsel
-(require 'counsel)
-(counsel-mode)
+(use-package smartparens
+  :ensure t
+  :hook (prog-mode text-mode)
+  :config (require 'smartparens-config))
 
-;; Dired
-(defun configure-dired () "Set up needed config for Dired."
-       (local-set-key (kbd "C-x C-f") 'find-file))
-(add-hook 'dired-mode-hook #'configure-dired)
+(use-package base16-theme
+  :ensure t
+  :config (load-theme 'base16-eighties t))
 
-;; Org
-(global-set-key (kbd "C-x M-o") 'org-agenda)
-(defun configure-org () "Set up needed config for Org mode."
-       (set-fill-column 80)
-       (auto-fill-mode))
-(add-hook 'org-mode-hook #'configure-org)
+(use-package mood-line
+  :ensure t
+  :config (mood-line-mode))
 
-;; ParEdit
-(autoload #'enable-paredit-mode "paredit" "Enable paredit." t)
-(add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
-(add-hook 'lisp-mode-hook #'enable-paredit-mode)
-
-;; GH-MD
-(autoload #'gh-md-render-buffer "gh-md" "Render buffer with GH-MD." t)
-(defun configure-gh-md () "Set up needed config for GH-MD."
-       (local-set-key (kbd "C-c C-c p") 'gh-md-render-buffer))
-(add-hook 'markdown-mode-hook #'configure-gh-md)
-
-;; Company
-(require 'company)
-(add-to-list 'company-backends 'company-tern)
-(global-company-mode)
-
-;; JavaScript
-(defun configure-javascript () "Set up Javascript-specific config."
-       (flycheck-mode 1)
-       (setq
-	indent-tabs-mode nil
-	js-indent-level 2))
-(add-hook 'javascript-mode-hook #'configure-javascript)
-(add-hook 'js2-mode-hook #'configure-javascript)
-
-;; JSX
-(defun run-react-native-menu () "Use adb to open React Native's dev menu."
-       (interactive)
-       (shell-command "adb shell input keyevent 82"))
-(defun configure-jsx () "Set up JSX-specific config."
-       (local-set-key (kbd "f5") #'run-react-native-menu))
-(add-hook 'rjsx-mode-hook #'configure-jsx)
-
-;; Theme
-(load-theme 'base16-eighties t)
+;; remove gui elements
 (menu-bar-mode -1)
 (tool-bar-mode -1)
-(toggle-scroll-bar -1)
-(add-to-list 'default-frame-alist '(font . "Roboto Mono Light-9"))
+(scroll-bar-mode -1)
+
+;; always follow symlinks
+(setq vc-follow-symlinks t)
+
+;; auto-fill in text buffers
+(add-hook 'text-mode-hook 'auto-fill-mode)
 
 ;; Backup files behavior
 (setq
  backup-by-copying t
  backup-directory-alist '(("." . "~/.emacs.d/tmp"))
  delete-old-versions t
- kept-new-versions 6
- kept-old-versions 2
- version-control t)
+ kept-new-versions 5)
 
-;; Auto-generated
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("9be1d34d961a40d94ef94d0d08a364c3d27201f3c98c9d38e36f10588469ea57" default)))
- '(icomplete-mode nil)
- '(org-agenda-files (quote ("~/org/")))
- '(package-selected-packages
-   (quote
-    (rjsx-mode company-tern counsel ivy swiper projectile indium js2-mode gh-md markdown-mode company base16-theme flycheck magit paredit slime))))
+ '(package-selected-packages '(base16-theme mood-line smartparens vertico)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
